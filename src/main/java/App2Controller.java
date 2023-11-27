b725f857ad16e33b917b405a2ddd9a2c187dc032
Cfournymanassevitz <carole.fourny@Digital-Grenoble.local>
2023-11-27 16:08:53 +0100

    il faut installer le ramdom

--- src/main/java/App2Controller.java
+++ src/main/java/App2Controller.java
@@ -1,9 +1,13 @@
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+import java.util.Random;
 import java.util.List;
 
 
 @RestController
 
 public class App2Controller {
+
+    @GetMapping
+    Random randomNumbers = new Random();
 }

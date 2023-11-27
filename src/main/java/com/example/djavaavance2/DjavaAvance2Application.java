03036b8d64b85796d21bb38b508fb8f646b837ff
Cfournymanassevitz <carole.fourny@Digital-Grenoble.local>
2023-11-27 15:40:18 +0100

    test

---
+++ src/main/java/com/example/djavaavance2/DjavaAvance2Application.java
@@ -0,0 +1,13 @@
+package com.example.djavaavance2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DjavaAvance2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DjavaAvance2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/djavaavance2/DjavaAvance2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.djavaavance2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DjavaAvance2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

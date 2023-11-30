1afe4e69a0fecc451f02100fda3982049f4c8a45
Cfournymanassevitz <carole.fourny@Digital-Grenoble.local>
2023-11-30 10:23:13 +0100

    installation JPA

--- src/main/java/com/example/djavaavance2/Controller/PerosControler.java
+++ src/main/java/com/example/djavaavance2/Controller/PerosControler.java
@@ -1,5 +1,12 @@
 package com.example.djavaavance2.Controller;
 
+import com.example.djavaavance2.PersoDAO;
+import com.example.djavaavance2.Personnage;
+import com.fasterxml.jackson.databind.ser.FilterProvider;
+import com.fasterxml.jackson.databind.ser.impl.SimpleBeanPropertyFilter;
+import com.fasterxml.jackson.databind.ser.impl.SimpleFilterProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.converter.json.MappingJacksonValue;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
@@ -8,6 +15,35 @@ import org.springframework.web.client.RestTemplate;
 @RestController
 public class PerosControler {
 
+    @Autowired
+
+    private PersoDAO persoDao;
+
+    //Récupérer la liste des produits
+    @RequestMapping(value = "/Produits", method = RequestMethod.GET)
+
+    public MappingJacksonValue listeProduits() {
+
+        Iterable<Personnage> personnages = persoDao.findAll();
+
+        SimpleBeanPropertyFilter monFiltre = SimpleBeanPropertyFilter.serializeAllExcept("prixAchat");
+
+        FilterProvider listDeNosFiltres = new SimpleFilterProvider().addFilter("monFiltreDynamique", monFiltre);
+
+        MappingJacksonValue personnagesFiltres = new MappingJacksonValue(personnages);
+
+
+        personnagesFiltres.setFilters(listDeNosFiltres);
+
+        return personnagesFiltres;
+    }
+
+    //Récupérer un produit par son Id
+ /* @GetMapping(value = "/Produits/{id}")
+  public Product afficherUnProduit(@PathVariable int id) {
+    return productDao.findById(id);
+
+  }
     private final RestTemplate restTemplate;
 
 
